56104f6599716528d98a031079d19f29d3e767a0
Tauhid Afser <74610615+nizs@users.noreply.github.com>
2022-03-14 22:59:35 +0600

    Update Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import './Footer.css'
 const Footer = () => {
     return (
         <div className="footer-container">
-            <p>© Tauhid Afser 2021, A Programming Hero initiative By Tauhid.</p>
+            <p>© Tauhid Afser 2022, A Programming Hero initiative By Tauhid.</p>
         </div>
     );
 };
